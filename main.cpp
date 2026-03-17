861bb5269f3006b3808b4e2a30603f299c39e320
tabecerra <tbecerra810@gmail.com>
2026-03-16 23:54:04 -0300

    Validacion del movimiento del peon.

--- main.cpp
+++ main.cpp
@@ -52,16 +52,42 @@ bool validMove(char board[8][8], Position origin, Position target) {
 	return true;
 }
 
+bool validMovePiece(char board[8][8], Position origin, Position target){
+	char piece = board[origin.row][origin.column];
+	switch(piece) {
+		case 'P':
+			if(board[target.row][target.column] == ' '){
+				if(origin.column == target.column && origin.row == 6 && (target.row == 5 || (target.row == 4 && board[5][target.column] == ' '))) {
+					return true;
+				} 
+				if(origin.row < 6 && target.row >= 0 && target.row - origin.row == -1 && origin.column == target.column) {
+					return true;
+				}
+			}
+		case 'p':
+		if(board[target.row][target.column] == ' '){
+			if(origin.column == target.column && origin.row == 1 && (target.row == 2 || (target.row == 3 && board[2][target.column] == ' '))) {
+				return true;
+			} 
+			if(origin.row > 1 && target.row <= 7 && target.row - origin.row == 1 && origin.column == target.column) {
+				return true;
+			}
+		}
+		default: 
+			return false;
+	}
+}
+
 int main() {
 	char board[8][8] = {
-		{'R','N','B','Q','K','B','N','R'},
-		{'P','P','P','P','P','P','P','P'},
-		{' ',' ',' ',' ',' ',' ',' ',' '},
-		{' ',' ',' ',' ',' ',' ',' ',' '},
-		{' ',' ',' ',' ',' ',' ',' ',' '},
-		{' ',' ',' ',' ',' ',' ',' ',' '},
+		{'r','n','b','q','k','b','n','r'},
 		{'p','p','p','p','p','p','p','p'},
-		{'r','n','b','q','k','b','n','r'}
+		{' ',' ',' ',' ',' ',' ',' ',' '},
+		{' ',' ',' ',' ',' ',' ',' ',' '},
+		{' ',' ',' ',' ',' ',' ',' ',' '},
+		{' ',' ',' ',' ',' ',' ',' ',' '},
+		{'P','P','P','P','P','P','P','P'},
+		{'R','N','B','Q','K','B','N','R'}
 	};
 	printBoard(board);
 	Position origen = {.column = 4, .row = 6};  // e2
